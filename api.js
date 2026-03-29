f90063bfbe0c001570c178b3275ff81b9f083107
Aditya Kumar <adityaforwork101@gmail.com>
2026-03-29 20:29:30 +0530

    api istho mschange

--- api.js
+++ api.js
@@ -4,20 +4,18 @@ let factPara = document.getElementById("fact");
 let btn = document.getElementById("button");
 
 let getFacts = async () => {
-
-    try{
-        factPara.innerHTML = "Loading..."
-        console.log("Getting data....");
-        let response = await fetch(URL);
-        let data = await response.json();
-        console.log(data);
-        factPara.innerHTML = data.fact;
-        btn.innerHTML = "Check Another One";
-    } catch (error) {
-        factPara.innerHTML = "Could not fetch a fact. Try again!";
-        console.error(error);
-    }
-   
-}
+  try {
+    factPara.innerHTML = "Loading...";
+    console.log("Getting data....");
+    let response = await fetch(URL);
+    let data = await response.json();
+    console.log(data);
+    factPara.innerHTML = data.fact;
+    btn.innerHTML = "Check Another One";
+  } catch (error) {
+    factPara.innerHTML = "Could not fetch a fact. Try again!";
+    console.error(error);
+  }
+};
 
 btn.addEventListener("click", getFacts);
